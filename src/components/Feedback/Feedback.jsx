db34250f538cdff294fc778bcf166fa5ae79b596
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-09-29 13:49:20 +0300

    add files

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,23 @@
+import { useLayoutEffect } from "react";
+import styles from "./Feedback.module.css";
+
+const Feedback = ({ feedback, totalFeedback, positiveFeedback }) => {
+  return (
+    <ul className={styles.wriper}>
+      {Object.entries(feedback).map((item, idx) => {
+        return (
+          <li className={styles.text} key={idx}>
+            {item[0]}: <b>{item[1]}</b>
+          </li>
+        );
+      })}
+      <li>
+        Total: <b> {totalFeedback}</b>
+      </li>
+      <li>
+        Positive: <b> {positiveFeedback}% </b>
+      </li>
+    </ul>
+  );
+};
+export default Feedback;
